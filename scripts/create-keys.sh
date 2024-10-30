c1039a8852c2cf64965afb668d530e1ac1dbfc33
Rod Laycock <rod.laycock@gmail.com>
2024-10-30 11:42:10 +0000

    added script to create SSH Keys

---
+++ scripts/create-keys.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function usage () {
+    APP_NAME=$(basename "$0")
+    cat <<EOT
+
+NAME
+
+   $APP_NAME
+
+USAGE
+
+   $APP_NAME USER SSH_KEY_NAME
+
+SYNOPSIS
+
+$APP_NAME will create a new SSH Key with the username specified
+
+EXAMPLE
+
+The following will create a new key private key called my-key and a public key called my-key.pub
+
+    $APP_NAME user my-key
+
+The following will create a new key private key called my-other-key and a public key called my-other-key.pub in the directory /keys/my-other-key
+
+    $APP_NAME user /keys/my-other-key
+
+EOT
+}
+
+if [ "$1" = "" ] || [ "$2" = "" ]; then
+    usage
+    exit 0
+else
+    if ! type "ssh-keygen" > /dev/null; then
+        echo "ssh-keygen command not found. Please install it."
+        return -1
+    else
+        ssh-keygen -C $1 -f $2
+    fi
+fi
